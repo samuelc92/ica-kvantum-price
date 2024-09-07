e6bc06ab3474abe820ae7c616b6990e4a007bc59
Samuel Couto <samuel.couto92@gmail.com>
2024-09-08 00:34:23 +0200

    Add the new class Shopping

---
+++ IcaKvantumPrice.ApiService/Domain/Shopping.cs
@@ -0,0 +1,14 @@
+﻿namespace IcaKvantumPrice.ApiService.Domain;
+
+public class Shopping
+{
+    public Guid Id { get; init; } = Guid.NewGuid();
+
+    public string ProductIdentifier { get; init; } = default!;
+
+    public string Description { get; init; } = default!;
+
+    public double Price { get; init; }
+
+    public DateTime ShoppingDate { get; init; }
+}
