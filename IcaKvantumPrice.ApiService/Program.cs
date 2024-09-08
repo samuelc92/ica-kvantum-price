51c4db4f594108751b8d99a0b0bcf59b745533f5
Samuel Couto <samuel.couto92@gmail.com>
2024-09-08 16:45:35 +0200

    Add and configure Serilog

--- IcaKvantumPrice.ApiService/Program.cs
+++ IcaKvantumPrice.ApiService/Program.cs
@@ -1,41 +1,66 @@
 using IcaKvantumPrice.ApiService.Database;
+using Serilog;
+using Serilog.Events;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-// Add service defaults & Aspire components.
-builder.AddServiceDefaults();
-
-// Add services to the container.
-builder.Services.AddProblemDetails();
-
-builder.AddNpgsqlDbContext<DatabaseContext>("icadb");
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-app.UseExceptionHandler();
-
-var summaries = new[]
+try
 {
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services.AddSerilog((services, lc) =>lc
+        .ReadFrom.Configuration(builder.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext()
+        .WriteTo.Console());
+
+    // Add service defaults & Aspire components.
+    builder.AddServiceDefaults();
+
+    // Add services to the container.
+    builder.Services.AddProblemDetails();
+
+    builder.AddNpgsqlDbContext<DatabaseContext>("icadb");
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    app.UseExceptionHandler();
+
+    var summaries = new[]
+    {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    };
 
-app.MapGet("/weatherforecast", () =>
+    app.MapGet("/weatherforecast", () =>
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+            new WeatherForecast
+            (
+                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Random.Shared.Next(-20, 55),
+                summaries[Random.Shared.Next(summaries.Length)]
+            ))
+            .ToArray();
+        return forecast;
+    });
+
+    app.MapDefaultEndpoints();
+
+    app.Run();
+}
+catch (Exception ex)
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
-
-app.MapDefaultEndpoints();
-
-app.Run();
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
