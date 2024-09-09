355218b8dc4848ae50d3eaf66ce72444ec885663
Samuel Couto <samuel.couto92@gmail.com>
2024-09-09 22:40:05 +0200

    Create ShoppingService class

---
+++ IcaKvantumPrice.ApiService/Services/ShoppingService.cs
@@ -0,0 +1,26 @@
+﻿using IcaKvantumPrice.ApiService.Database;
+using IcaKvantumPrice.ApiService.Domain;
+using Microsoft.EntityFrameworkCore;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace IcaKvantumPrice.ApiService.Services;
+
+public class ShoppingService(DatabaseContext database, IPdfService pdfService) : IShoppingService
+{
+    public async Task CreateAsync(Shopping shopping)
+    {
+        var existingShopping = await database.Shoppings.Where(p => p.Id == shopping.Id).FirstOrDefaultAsync();
+        if (existingShopping is not null)
+        {
+            var message = $"A shopping with id {shopping.Id} already exists";
+            throw new ValidationException(message,
+            [
+                new ValidationFailure(nameof(Shopping), message)
+            ]);
+        }
+
+        database.Shoppings.Add(shopping);
+        await database.SaveChangesAsync();
+    }
+}
