3871e5d762ec4af1ff657687db409378e4e9ebe9
Samuel Couto <samuel.couto92@gmail.com>
2024-09-10 21:25:10 +0200

    Create new shopping page

---
+++ IcaKvantumPrice.Web/Components/Custom/AddShopping.razor.cs
@@ -0,0 +1,25 @@
+﻿using IcaKvantumPrice.Web.ViewModels;
+using Microsoft.AspNetCore.Components;
+
+namespace IcaKvantumPrice.Web.Components.Custom;
+
+public partial class AddShopping
+{
+
+    [Inject]
+    public ShoppingApiClient ApiClient { get; set; }
+
+
+    [SupplyParameterFromForm]
+    private ShopViewModel _shopViewModel { get; set; } = new();
+
+    protected override void OnInitialized()
+    {
+        _shopViewModel.ShoppingDate = DateTime.Now;
+    }
+
+    private async Task HandleValidSubmit()
+    {
+        await ApiClient.PostShopping(_shopViewModel);
+    }
+}
---
+++ IcaKvantumPrice.Web/Components/Pages/Shop.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.FluentUI.AspNetCore.Components;
+
+namespace IcaKvantumPrice.Web.Components.Pages;
+
+public partial class Shop
+{
+    string? activeid = "tab-1";
+    FluentTab? changedto;
+
+    private void HandleOnTabChange(FluentTab tab)
+    {
+        changedto = tab;
+    }
+}
