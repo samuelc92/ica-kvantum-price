3dd4716f1f6da2602497c8e0d13de8f2aa0a290a
Samuel Couto <samuel.couto92@gmail.com>
2024-09-15 22:14:18 +0200

    Update Home.razor.cs

--- IcaKvantumPrice.Web/Components/Pages/Home.razor.cs
+++ IcaKvantumPrice.Web/Components/Pages/Home.razor.cs
@@ -1,5 +1,6 @@
 ﻿using IcaKvantumPrice.Web.ViewModels;
 using Microsoft.AspNetCore.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
 
 namespace IcaKvantumPrice.Web.Components.Pages;
 
@@ -8,11 +9,22 @@ public partial class Home
     [Inject]
     public ShoppingApiClient ApiClient { get; set; }
 
+    [Inject]
+    public IToastService ToastService { get; set; }
+
+
     private IQueryable<ProductPriceReportViewModel>? _report;
 
     protected override async Task OnInitializedAsync()
     {
-        var report = await ApiClient.GetProductPriceReportAsync();
-        _report = report?.AsQueryable();
+        try
+        {
+            var report = await ApiClient.GetProductPriceReportAsync();
+            _report = report?.AsQueryable();
+        }
+        catch
+        {
+            ToastService.ShowError("Failed on fetching product's prices.");
+        }
     }
 }
