66c06a11cdf47d0fd8a962768317f119aefeb722
Samuel Couto <samuel.couto92@gmail.com>
2024-09-17 21:40:33 +0200

    Update StoreViewModel.cs

--- IcaKvantumPrice.Web/ViewModels/StoreViewModel.cs
+++ IcaKvantumPrice.Web/ViewModels/StoreViewModel.cs
@@ -4,7 +4,7 @@ namespace IcaKvantumPrice.Web.ViewModels;
 
 public class StoreViewModel
 {
-    public int ProductIdentifier { get; set; } = 0;
+    public Guid Id { get; set; }
 
     [MaxLength(20, ErrorMessage = "Description too long (20 character limit")]
     public string? Name { get; set; }
