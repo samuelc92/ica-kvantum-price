42bf1690848849144b27e9cf1ef50a9779f0d8b3
Samuel Couto <samuel.couto92@gmail.com>
2024-09-18 22:17:11 +0200

    Create GetPriceReportSummary.cs

---
+++ IcaKvantumPrice.ApiService/Summaries/GetPriceReportSummary.cs
@@ -0,0 +1,16 @@
+﻿using FastEndpoints;
+using IcaKvantumPrice.ApiService.Endpoint;
+using IcaKvantumPrice.ApiService.Services;
+
+namespace IcaKvantumPrice.ApiService.Summaries
+{
+    public class GetPriceReportSummary : Summary<GetPriceReportEndpoint>
+    {
+        public GetPriceReportSummary()
+        {
+            Summary = "Get prices report";
+            Description = "Get prices report";
+            Response<ICollection<ProductPriceReport>>(200, "Collection of prices");
+        }
+    }
+}
